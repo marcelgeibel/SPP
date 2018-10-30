2891bb6235ca4e90028654c4061fc277b0a22453
dangelo <laura.dangelo@web.de>
2018-10-30 18:14:51 +0100

    Übung 1, Aufgabe 2: source code korrigiert, Fehler als Kommentare und als printf-Text eingefügt, makefile hochgeladen.

--- uebung1/seq_fault.c
+++ uebung1/seq_fault.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 typedef struct
 {
@@ -14,10 +15,10 @@ create_student( char* name,
 		int semester,
 		char* program )
 {
-  student* new_student;
-  new_student->name    = name;
-  new_student.matriculation_number    = matriculation_number;
-  new_student.semester = semester;
+  student* new_student = malloc(sizeof(student)); // es wurde kein Speicherplatz für new_student allokiert
+  new_student->name = name;
+  new_student->matriculation_number = matriculation_number; // um bei einem struct-Pointer auf ein member zuzugreifen, benutzt man den -> Operator, nicht den . Operator
+  new_student->semester = semester; // gleicher Fehler wie in der vorherigen Zeile
   new_student->program  = program;
   return new_student;
 }
@@ -26,5 +27,10 @@ int main()
 {
   student* s1 = create_student( "Max Mustermann", 424242, 1, "Computer Science" );
   free( s1 );
+  printf("Bei den drei Fehlern handelte es sich um:\n");
+  printf("Zeile 17 (Laufzeitfehler): Es wurde kein Speicher für new_student allokiert. Behebung durch malloc.\n");
+  printf("Zeile 19 (Kompilierfehler): Für Zugriff auf members von struct pointers nutzt man den Pfeil-Operator ->.\n");
+  printf("Der Punktoperator hingegen wird benutzt beim Zugriff auf members von structs (nicht Pointer auf structs!).\n");
+  printf("Zeile 20 (Kompilierfehler): Gleicher Fehler wie in Zeile 19. Behebung ebenso mit Pfeil- statt Punktoperator.\n");
 }
 
